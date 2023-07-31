3e7404f457adce49ef34e226a8be2d23f9317831
Janaka Daskon <janakadaskon@gmail.com>
2023-07-31 18:50:11 +0530

    Finished Task5

---
+++ Demo5.java
@@ -0,0 +1,13 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        int myInt1 = 978;
+        int myInt2 = 3;
+        int myInt3 = 16;
+        int myInt4 = 1484100;
+
+        System.out.printf("\033[31m%d\033[0m-\033[32m%d\033[0m-\033[33m%d\033[0m-\033[034m%d\033[0m \n", myInt1, myInt2, myInt3, myInt4);
+
+
+    }
+    
+}
